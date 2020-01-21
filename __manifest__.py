e9bce4b50e478a13167d85c0f3a8a5b8ecd3c1a8
gamhpang <gamhpang1812@gmail.com>
2020-01-21 14:37:23 +0700

    First Commit

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    "name":"Unique product name",
+    "summary":"Set product name as Unique",
+    "version" : "13.0",
+    "author" : "Gam Hpang, Safecoms Company Limited",
+    "category" : "Product",
+    "website" : "https://safecoms.com",
+    "depends" : ["product"],
+    "installable" : True,
+}
