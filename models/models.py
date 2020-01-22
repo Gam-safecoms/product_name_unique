fb97d8664a9f9ca44849abcab2308fcd2f8061e6
gamhpang <gamhpang1812@gmail.com>
2020-01-22 16:39:28 +0700

    update for edit

--- models/models.py
+++ models/models.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 
 # from odoo import models, fields, api
-
-import logging
 from odoo import api,_
 from odoo.exceptions import ValidationError
 from odoo import models
 
-_logger = logging.getLogger(__name__)
 
 class ProductProduct(models.Model):
 	_inherit = 'product.product'
@@ -15,8 +12,19 @@ class ProductProduct(models.Model):
 
 	@api.model_create_multi
 	def create(self,vals_list):
-		_logger.info("in product name unique module .......")
 		products = super(ProductProduct, self.with_context(create_product_product=True)).create(vals_list)
 		matching_products = self.env['product.product'].search([('name','=',products.name)])
 		if len(matching_products)>1:
 			raise ValidationError(_('Product name must be unique!'))
+
+class ProductTemplate(models.Model):
+	_inherit = 'product.template'
+
+	def write(self,vals):
+		res = super(ProductTemplate, self).write(vals)
+		matching_products = self.env['product.product'].search([('name','=',vals['name'])])
+		print(matching_products)
+		if len(matching_products) > 1:
+			raise ValidationError(_('Product name must be unique!'))
+
+
